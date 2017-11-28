8a09391da0eb1dc76c8352c4af09a447586aa98b
Ross van Zyl <rossvz@getsellr.com>
2017-11-27 22:04:06 -0500

    cleaned up isUpvoted logic

--- src/components/Song/Upvote.js
+++ src/components/Song/Upvote.js
@@ -2,20 +2,16 @@ import { contains } from 'ramda'
 import React from 'react'
 import { readVotesFromStorage } from 'util/votes'
 
-const isUpvoted = songId => {
-  let persistedVotes = readVotesFromStorage()
-  return contains(songId, persistedVotes)
-}
-
 const Upvote = ({changeVote, songId}) => {
+  const isUpvoted = contains(songId, readVotesFromStorage())
   const styles = {
     arrowStyles: {
       width: '50px',
-      fill: isUpvoted(songId) ? 'orangered' : ''
+      fill: isUpvoted ? 'orangered' : ''
     }
   }
   return (
-    <div onClick={() => changeVote(isUpvoted(songId) ? -1 : 1)}
+    <div onClick={() => changeVote(isUpvoted ? -1 : 1)}
          style={styles.arrowStyles}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 640">
         <path d="M128 320l128-128 128 128" />
