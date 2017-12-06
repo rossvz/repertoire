fedf5ff165ec3e44f7038c947edaf504ee7a92ea
Ross van Zyl <rossvz@getsellr.com>
2017-12-05 21:50:43 -0500

    started on login code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
-const Login = ({email, password, emailChanged, passwordChanged, loginUser}) => {
+const Login = (props) => {
   const onSubmit = e => {
     e.preventDefault()
-    debugger
-    loginUser(email, password)
+    const credentials = {
+      email: props.authentication.email,
+      password: props.authentication.password
+    }
+    props.firebase.login(credentials)
+      .then(res => console.log('USER AUTHENTICATED!!', res))
+      .catch(err => console.error(err))
   }
 
   return (
     <div>
       <form onSubmit={e => onSubmit(e)}>
-        <input type="email" value={email} onChange={e => emailChanged(e.target.value)} />
-        <input type="password" value={password} onChange={e => passwordChanged(e.target.value)} />
+        <input type="email" value={props.authentication.email} onChange={e => props.emailChanged(e.target.value)} />
+        <input type="password" value={props.authentication.password} onChange={e => props.passwordChanged(e.target.value)} />
         <button type="submit">Login</button>
       </form>
     </div>
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux'
 import Login from './Login'
 import { emailChanged, passwordChanged, loginUser } from '../../store/auth/actions'
+import { firebaseConnect } from 'react-redux-firebase'
 
+const wrappedLogin = firebaseConnect([
+  'auth'
+])(Login)
 export default connect(
-  (state) => {
-    const {email, password, error, loading} = state.auth
-    return {email, password, error, loading}
-  },
+  ({firebase, auth}) => ({
+    auth: firebase.auth,
+    authentication: auth
+  }),
   {emailChanged, passwordChanged, loginUser}
-)(Login)
+)(wrappedLogin)
+
