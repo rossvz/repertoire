1d79e8c65a8fc1be680f501318967cfc2d91f34b
Ross van Zyl <rossvz@getsellr.com>
2017-12-28 23:16:06 -0500

    tweaking header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Header.css'
 
 const Header = ({title}) => {
   return (
@@ -13,7 +12,7 @@ const styles = {
   headerStyles: {
     width: '100%',
     height: '8vh',
-    // background: '#FC3A82',
+    background: 'transparent',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
