ad8d429caf27e56b8eb4e72afede431767268fd0
Ross van Zyl <rossvz@getsellr.com>
2018-03-12 21:23:14 -0400

    :hammer: (prettier) using prettier for formatting

--- src/components/SongPage/NewSong/NewSongForm/NewSongForm.js
+++ src/components/SongPage/NewSong/NewSongForm/NewSongForm.js
@@ -3,7 +3,15 @@ import { withFirebase } from 'react-redux-firebase'
 import { searchSpotify } from 'util/Spotify'
 import Button from 'components/common/Button'
 
-const INITIAL_STATE = { title: '', artist: '', album: '', votes: 0, results: [], resultIndex: 0, visible: true }
+const INITIAL_STATE = {
+  title: '',
+  artist: '',
+  album: '',
+  votes: 0,
+  results: [],
+  resultIndex: 0,
+  visible: true
+}
 
 class NewSongForm extends Component {
   constructor(props) {
@@ -23,18 +31,26 @@ class NewSongForm extends Component {
       results: results.tracks.items
     })
     this.nextResult(false)
-
   }
 
   saveSong() {
     const { title, artist, album, votes, albumArtwork, visible } = this.state
-    this.props.firebase.push('/songs', { title, artist, album, votes, albumArtwork, visible })
+    this.props.firebase.push('/songs', {
+      title,
+      artist,
+      album,
+      votes,
+      albumArtwork,
+      visible
+    })
     this.props.toggleIsEditing()
     this.setState(INITIAL_STATE)
   }
 
   nextResult(increment = true) {
-    const index = increment ? this.state.resultIndex + 1 : this.state.resultIndex
+    const index = increment
+      ? this.state.resultIndex + 1
+      : this.state.resultIndex
     const result = this.state.results[index]
     if (!result) return
     this.setState({
@@ -79,36 +95,70 @@ class NewSongForm extends Component {
   }
 
   renderNextResult() {
-    if (this.state.resultIndex + 1 < this.state.results.length) return <Button onClick={this.nextResult.bind(this)}>NEXT</Button>
-    else return <Button disabled={true} style={styles.disabled}>NEXT</Button>
+    if (this.state.resultIndex + 1 < this.state.results.length)
+      return (
+        <Button type="button" onClick={this.nextResult.bind(this)}>
+          NEXT
+        </Button>
+      )
+    else
+      return (
+        <Button type="button" disabled={true} style={styles.disabled}>
+          NEXT
+        </Button>
+      )
   }
 
   renderPreviousResult() {
-    if (this.state.resultIndex > 0) return <Button onClick={this.previousResult.bind(this)}>PREV</Button>
-    else return <Button disabled={true} style={styles.disabled}>PREV</Button>
+    if (this.state.resultIndex > 0)
+      return (
+        <Button type="button" onClick={this.previousResult.bind(this)}>
+          PREV
+        </Button>
+      )
+    else
+      return (
+        <Button type="button" disabled={true} style={styles.disabled}>
+          PREV
+        </Button>
+      )
   }
 
   render() {
     return (
       <div style={styles.newSongContainer}>
-      <div style={styles.closeButton}>
-      <Button onClick={this.cancelSong.bind(this)}>X</Button>
-
-      </div>
+        <div style={styles.closeButton}>
+          <Button onClick={this.cancelSong.bind(this)}>X</Button>
+        </div>
         <form onSubmit={this.searchSong.bind(this)} style={styles.formStyles}>
-          <input style={styles.inputStyles} type="text" placeholder={'Title'} onChange={this.onTitleChange.bind(this)} value={this.state.title} />
-          <input style={styles.inputStyles} type="text" placeholder={'Artist (optional)'} onChange={this.onArtistChange.bind(this)}
-            value={this.state.artist} />
+          <input
+            style={styles.inputStyles}
+            type="text"
+            placeholder={'Title'}
+            onChange={this.onTitleChange.bind(this)}
+            value={this.state.title}
+          />
+          <input
+            style={styles.inputStyles}
+            type="text"
+            placeholder={'Artist (optional)'}
+            onChange={this.onArtistChange.bind(this)}
+            value={this.state.artist}
+          />
           <div style={styles.inputStyles}>{this.state.album}</div>
           <div style={styles.findButtonContainer}>
             {this.renderPreviousResult()}
             <Button type="submit">SEARCH</Button>
             {this.renderNextResult()}
           </div>
-
         </form>
         <div style={styles.results}>
-          <img height="100px" width="100px" src={this.state.albumArtwork} alt="" />
+          <img
+            height="100px"
+            width="100px"
+            src={this.state.albumArtwork}
+            alt=""
+          />
         </div>
         <div style={styles.buttonContainer}>
           <Button onClick={this.saveSong.bind(this)}>SAVE</Button>
@@ -120,14 +170,14 @@ class NewSongForm extends Component {
 
 const styles = {
   newSongContainer: {
-    border:'2px solid white',
+    border: '2px solid white',
     borderRadius: '5px',
     position: 'fixed',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     background: 'white',
-    zIndex:'2',
+    zIndex: '2',
     boxShadow: 'rgba(0, 0, 0, 0.75) -1px 4px 20px 8px'
   },
   formStyles: {
@@ -155,7 +205,6 @@ const styles = {
   results: {
     display: 'flex',
     justifyContent: 'space-evenly'
-
   },
   disabled: {
     opacity: 0.5
@@ -165,16 +214,16 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'space-around'
   },
-  findButtonContainer:{
-    display:'flex',
-    flexFlow:'row nowrap',
-    alignItems:'center',
-    justifyContent:'space-evenly',
-    width:'85vw'
+  findButtonContainer: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: '85vw'
   },
-  closeButton:{
-    display:'flex',
-    justifyContent:'flex-end'
+  closeButton: {
+    display: 'flex',
+    justifyContent: 'flex-end'
   }
 }
 
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 const styles = {
-  buttonStyles:{
+  buttonStyles: {
     fontSize: '1em',
     padding: '10px',
     borderRadius: '100em',
     background: '#f8f8f8',
     color: '#333'
-  },
+  }
 }
 
 const Button = props => (
-  <button style={styles.buttonStyles} {...props}>{props.children}</button>
+  <button style={styles.buttonStyles} {...props}>
+    {props.children}
+  </button>
 )
 
 export default Button
