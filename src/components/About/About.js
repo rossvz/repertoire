dea8e0e2a39861389e1f038d5e3e367af8c8a32c
randygodwin <36388649+randygodwin@users.noreply.github.com>
2018-03-18 20:02:13 -0400

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ const About = props => {
 
       <h2>Randy Godwin</h2>
       <div>
-        <p>From the bottom of the page, press "Songs" to view the song list I'll be playing from -or- press "Shows" to view my upcoming events.  If you are listening live, I invite you to vote for your favorite songs by pressing the "like" image next to the song.  Throughout the show, I will play the most popular song requests.</p>
+        <p>From the bottom of the page, press "Songs" to view the song list I will be playing from -or- press "Shows" to view my upcoming events.  If you are listening live, I invite you to vote for your favorite songs by pressing the "like" image next to the song.  Throughout the show, I will play the most popular song requests.</p>
       </div>
     </div>
   )
