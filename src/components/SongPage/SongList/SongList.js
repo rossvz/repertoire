87c34002073bd163c343543a1683a87b905a972d
Ross van Zyl <ross@instinct.vet>
2024-02-04 22:52:53 -0500

    reset search

--- src/components/SongPage/SongList/SongList.js
+++ src/components/SongPage/SongList/SongList.js
@@ -4,6 +4,7 @@ import { useSigninCheck } from "reactfire"
 
 import Song from "./Song/Song"
 import { useSongs } from "./useSongs"
+import FontAwesome from "react-fontawesome"
 
 export const SongList = () => {
   const { status: authStatus, data: user } = useSigninCheck()
@@ -14,14 +15,30 @@ export const SongList = () => {
     return <div>Loading...</div>
   }
 
+  const resetSearch = () => {
+    setSearchFilter("")
+  }
+
   return (
     <>
       <SearchContainer>
         <SearchInput
           onChange={(e) => setSearchFilter(e.target.value)}
-          placeholder="Search songs by title, artist, or album..."
+          placeholder="Search title or artist"
           value={searchFilter}
         />
+        <div onClick={resetSearch}>
+          <FontAwesome
+            name="times-circle"
+            style={{
+              color: "white",
+              fontSize: "1.8em",
+              fontWeight: "bold",
+              cursor: "pointer",
+              margin: "0 10px",
+            }}
+          />
+        </div>
       </SearchContainer>
       <div style={styles.songListStyles}>
         {songs.map((song) => (
@@ -48,17 +65,18 @@ const SearchContainer = styled.div`
   margin: 2% 0;
   display: flex;
   flex-flow: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  flex-direction: row;
 `
 
 const SearchInput = styled.input`
   text-align: left;
   font-size: 1.3em;
-  //background-color: #19181c;
   background-color: #161519;
   color: white;
-  border: 2px solid #b4cbea;
+  border: none;
+  borderbottom: 2px solid #b4cbea;
   padding: 20px;
-  width: 90%;
+  width: 85%;
 `
