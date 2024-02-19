b0274e1eb41a1ecbd019b3c2fe35c1f1eb7d18a5
Ross van Zyl <ross@instinct.vet>
2024-02-19 17:03:58 -0500

    update react router to v5.1

--- src/App.js
+++ src/App.js
@@ -5,28 +5,39 @@ import { getAuth } from "firebase/auth"
 import "fontsource-roboto/latin.css"
 import "./App.css"
 
-import { Router, Route } from "react-router-dom"
-import history from "./util/history"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+// import history from "./util/history"
 import About from "./components/About/About"
 import Footer from "./components/Footer/Footer"
 import { ShowsPage } from "./components/Shows/ShowsPage"
 import { SongListPage } from "./components/SongPage/SongPage"
 import { AdminPage } from "./components/Admin/AdminPage"
+import Song from "./components/SongPage/SongList/Song/Song"
 
 const App = () => {
   const firebaseApp = useFirebaseApp()
   const auth = getAuth(firebaseApp)
   return (
     <AuthProvider sdk={auth}>
-      <Router history={history}>
+      <BrowserRouter>
         <div className="App" style={responsiveStyles}>
-          <Route exact path="/" component={SongListPage} />
-          <Route path="/about" component={About} />
-          <Route exact path="/shows" component={ShowsPage} />
-          <Route path="/admin" component={AdminPage} />
+          <Switch>
+            <Route exact path="/">
+              <SongListPage />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route exact path="/shows">
+              <ShowsPage />
+            </Route>
+            <Route path="/admin">
+              <AdminPage />
+            </Route>
+          </Switch>
           <Footer />
         </div>
-      </Router>
+      </BrowserRouter>
     </AuthProvider>
   )
 }
@@ -35,8 +46,8 @@ const styles = {
   app: {
     // marginLeft: "20vw",
     // marginRight: "20vw",
-    boxShadow: "0px 2px 19px 8px black"
-  }
+    boxShadow: "0px 2px 19px 8px black",
+  },
 }
 
 const responsiveStyles = window.innerWidth > 700 ? styles.app : {}
