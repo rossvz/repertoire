32a0e74adb7725707d8557644f66ce658c3fdd1a
Ross van Zyl <ross@instinct.vet>
2024-02-22 08:56:27 -0500

    update About page, add instagram button

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,25 +1,41 @@
 import React from "react"
 import { Header } from "../Header"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
+import Button from "../common/Button"
 
-const About = props => {
+const About = (props) => {
   return (
     <div style={styles.aboutContainer}>
       <Header title={"About Me"} />
-      <h2>Randy Godwin</h2>
-      <div
-        style={{
-          paddingLeft: "3em",
-          paddingRight: "3em"
-        }}
-      >
+      <div style={{ paddingLeft: "3em", paddingRight: "3em" }}>
         <p>
-          I am a singer & guitar player in the Atlanta Georgia area where I
+          I am a singer & guitar player in the Atlanta, Georgia area where I
           perform acoustic covers at local restaurants and private events. Thank
           you for using this app! I hope it will be a fun way for us to interact
           during our time together. Grab some food, pull up a chair, and let's
           sing some songs.
         </p>
+        <p>-- Randy Godwin</p>
       </div>
+
+      <a
+        style={{
+          marginTop: "2em",
+          display: "flex",
+          justifyContent: "center",
+          textDecoration: "none",
+        }}
+        href="https://www.instagram.com/randy.godwin.music/?utm_source=ig_embed&utm_campaign=loading"
+        target="_blank"
+      >
+        <Button>
+          <div style={styles.instagram}>
+            <span>Follow me on Instagram</span>
+            <FontAwesomeIcon style={styles.instagramIcon} icon={faInstagram} />
+          </div>
+        </Button>
+      </a>
     </div>
   )
 }
@@ -27,13 +43,24 @@ const About = props => {
 const styles = {
   aboutContainer: {
     color: "white",
-
-    textAlign: "justify"
+    textAlign: "justify",
   },
   image: {
     width: "100%",
-    height: "auto"
-  }
+    height: "auto",
+  },
+  instagram: {
+    fontSize: "20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textDecoration: "none",
+  },
+  instagramIcon: {
+    fontSize: "30px",
+    color: "white",
+    marginLeft: "5px",
+  },
 }
 
 export default About
