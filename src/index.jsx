30f7867144fc4a199b9e1069cfaef105eec92256
Ross van Zyl <ross@instinct.vet>
2025-03-02 20:30:52 -0500

    tweaking firebase github action

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,17 @@ import "./index.css"
 
 import App from "./App"
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
-  authDomain: "rg-music.firebaseapp.com",
-  databaseURL: "https://rg-music.firebaseio.com",
-  projectId: "rg-music",
-  storageBucket: "rg-music.appspot.com",
-  appId: "390024634944",
-  messagingSenderId: "390024634944",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:
+    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "rg-music.firebaseapp.com",
+  databaseURL:
+    import.meta.env.VITE_FIREBASE_DATABASE_URL ||
+    "https://rg-music.firebaseio.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "rg-music",
+  storageBucket:
+    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "rg-music.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   preserveOnLogout: { data: ["songs"] },
   preserveOnEmptyAuthChange: { data: ["songs"] },
 }
